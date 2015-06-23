eb0ba701da6391d1aaa8f178cfb64a9af9458620
Matthew Ronemous <matt.ronemous@gmail.com>
2015-06-23 17:51:44 -0400

    initial map integration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,29 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+
+	var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+
+	var mapOptions = {
+		center: myLatlng,
+		zoom: 16,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: 'Uluru (Ayers Rock)'
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(map,marker);
+	});
+
+	$scope.map = map;
+	
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
