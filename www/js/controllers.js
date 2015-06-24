c488875517fc72ff166f3c1e5d9d0612e49c94d4
jw1121 <wonwooim@gmail.com>
2015-06-24 08:03:56 -0400

    refresh refresh

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,73 +1,77 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $http, Filters) {
-   
-	  $scope.$on('$ionicView.enter', function(e) {    
-			loadData();
-		});
-		
-	
-	var heatmap;
-//    // start PloyLine sample
-//    var polyLineData = [
-//        new google.maps.LatLng(37.773379, -122.440542),
-//        new google.maps.LatLng(37.769308, -122.408098),
-//        new google.maps.LatLng(37.754516, -122.409299),
-//        new google.maps.LatLng(37.752208, -122.44758),
-//        new google.maps.LatLng(37.773379, -122.440542)
-//    ];
-//    var polyPath = new google.maps.Polyline({
-//        path: polyLineData,
-//        strokeColor: "#000000",
-//        strokeOpacity: 0.8,
-//        strokeWeight: 2
-//    });
-//
-//    polyPath.setMap(map);
-//    // end PloyLine sample
 
-//    google.maps.event.addListener(marker, 'click', function() {
-//
-//
-//    });
-	
-	
-		function loadData(bounds) {
-			
-			//alert(JSON.stringify(bounds));			
-			
-			var filters = Filters.all();
-			
-			$http({url: CONFIG.peopleApi + '/index.php', method: "GET", params: filters}).then(function(resp) {
-					console.log('Success', resp);
+    $scope.$on('$ionicView.enter', function(e) {
+        loadData();
+    });
 
-					refreshMap(resp.data);
 
-			}, function(err) {
-					console.error('ERR', err);
+    var heatmap;
+    //    // start PloyLine sample
+    //    var polyLineData = [
+    //        new google.maps.LatLng(37.773379, -122.440542),
+    //        new google.maps.LatLng(37.769308, -122.408098),
+    //        new google.maps.LatLng(37.754516, -122.409299),
+    //        new google.maps.LatLng(37.752208, -122.44758),
+    //        new google.maps.LatLng(37.773379, -122.440542)
+    //    ];
+    //    var polyPath = new google.maps.Polyline({
+    //        path: polyLineData,
+    //        strokeColor: "#000000",
+    //        strokeOpacity: 0.8,
+    //        strokeWeight: 2
+    //    });
+    //
+    //    polyPath.setMap(map);
+    //    // end PloyLine sample
 
-			});	
-		}
-	
-		function refreshMap(data) {
-		  
-			var heatData = [];
-			for(var i=0; i < data.length; i++) {
-				heatData.push(new google.maps.LatLng(data[i][1], data[i][0]));
-			}
-            
-            if (!!heatmap && !!heatmap.setMap)
+    //    google.maps.event.addListener(marker, 'click', function() {
+    //
+    //
+    //    });
+
+
+    function loadData(bounds) {
+
+        //alert(JSON.stringify(bounds));			
+
+        var filters = Filters.all();
+
+        $http({
+            url: CONFIG.peopleApi + '/index.php',
+            method: "GET",
+            params: filters
+        }).then(function(resp) {
+            console.log('Success', resp);
+
+            refreshMap(resp.data);
+
+        }, function(err) {
+            console.error('ERR', err);
+
+        });
+    }
+
+    function refreshMap(data) {
+
+        var heatData = [];
+        for (var i = 0; i < data.length; i++) {
+            heatData.push(new google.maps.LatLng(data[i][1], data[i][0]));
+        }
+
+        if (!!heatmap)
             heatmap.setMap(null);
-            
-            heatmap = new google.maps.visualization.HeatmapLayer({
-					data: heatData,
-					radius: 20,
-					opacity: 0.4
-			});
-			heatmap.setMap(map);	
-		}
-	
-	var sanFrancisco = new google.maps.LatLng(39, -95);
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatData,
+            radius: 20,
+            opacity: 0.4
+        });
+        heatmap.setMap(map);
+    }
+
+    var sanFrancisco = new google.maps.LatLng(39, -95);
     var mapOptions = {
         center: sanFrancisco,
         zoom: 5,
@@ -75,50 +79,50 @@ angular.module('starter.controllers', [])
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-		google.maps.event.addListener(map, 'bounds_changed', (function () {
-			  //loadData(map.getBounds());	
-		}));
-			
+    google.maps.event.addListener(map, 'idle', (function() {
+        //loadData(map.getBounds());	
+    }));
+
     var marker = new google.maps.Marker({
         position: sanFrancisco,
         map: map,
         title: 'Uluru (Ayers Rock)'
     });
 
-		var radarLayer = new google.maps.ImageMapType({
-			getTileUrl: function(tile, zoom) {
+    var radarLayer = new google.maps.ImageMapType({
+        getTileUrl: function(tile, zoom) {
 
-//				// convert the tile into a quad key
-//				var quadKey = "";
-//				for (var level = zoom; level > 0; level--) {
-//						var mask = 1 << (level-1);
-//
-//						var xMask = (tile.x & mask) != 0;
-//						var yMask = (tile.y & mask) != 0;
-//						if (xMask && yMask) {
-//								quadKey += '3';
-//						} else if (yMask) {
-//								quadKey += '2';
-//						}
-//						else if (xMask) {
-//								quadKey += '1';
-//						} else {
-//								quadKey += '0';
-//						}
-//
-//				}
+            //				// convert the tile into a quad key
+            //				var quadKey = "";
+            //				for (var level = zoom; level > 0; level--) {
+            //						var mask = 1 << (level-1);
+            //
+            //						var xMask = (tile.x & mask) != 0;
+            //						var yMask = (tile.y & mask) != 0;
+            //						if (xMask && yMask) {
+            //								quadKey += '3';
+            //						} else if (yMask) {
+            //								quadKey += '2';
+            //						}
+            //						else if (xMask) {
+            //								quadKey += '1';
+            //						} else {
+            //								quadKey += '0';
+            //						}
+            //
+            //				}
 
-				//return 'http://hackathon.weather.com/Maps/imgs/radar/' + '201506240150' + '/' + quadKey + '.png';
-				return "http://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime(); 
+            //return 'http://hackathon.weather.com/Maps/imgs/radar/' + '201506240150' + '/' + quadKey + '.png';
+            return "http://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/" + zoom + "/" + tile.x + "/" + tile.y + ".png?" + (new Date()).getTime();
 
-			},
-			tileSize: new google.maps.Size(256, 256),
-			opacity:0.60,
-			name : 'NEXRAD',
-			isPng: true,
-		});		
-		map.overlayMapTypes.push(radarLayer);
-	
+        },
+        tileSize: new google.maps.Size(256, 256),
+        opacity: 0.60,
+        name: 'NEXRAD',
+        isPng: true,
+    });
+    map.overlayMapTypes.push(radarLayer);
+
     $scope.map = map;
 
 })
